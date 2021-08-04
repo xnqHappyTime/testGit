dab6eba5a1333ac5d3e0e413fec452265016a353
xnqHappyTime <a1344194154@126.com>
2021-08-04 22:59:16 +0800

    添加文件

---
+++ HelloGit/src/main/java/HelloGit.java
@@ -0,0 +1,9 @@
+/**
+ * @author xiaonaiquan
+ * @create 2021-08-04-22:51
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
